d78bb184c79bc5c163bf0dfdb94d9f3263993243
firdavsyusupov <firdavsyusupov.pc.uz@gmail.com>
2025-07-29 15:46:54 +0500

    It works don't touch it

--- sales/serializers.py
+++ sales/serializers.py
@@ -12,11 +12,11 @@ class OutcomeItemSerializer(serializers.ModelSerializer):
 
 
 class OutcomeSerializer(serializers.ModelSerializer):
-    product_list = OutcomeItemSerializer(many=True, write_only=True)
-    sold_sum_price = serializers.DecimalField(max_digits=12, decimal_places=2)#, read_only=True)
-    stock_sum_price = serializers.DecimalField(max_digits=12, decimal_places=2)#, read_only=True)
-    profit = serializers.DecimalField(max_digits=12, decimal_places=2)#, read_only=True)
-    paid = serializers.BooleanField() 
+    product_list = OutcomeItemSerializer(many=True)
+    sold_sum_price = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
+    stock_sum_price = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
+    profit = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
+    paid = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Outcome
@@ -25,7 +25,7 @@ class OutcomeSerializer(serializers.ModelSerializer):
             'sold_sum_price', 'stock_sum_price', 'debt',
             'profit', 'paid', 'received_profit', 'comment', 'created_at', 'updated_at'
         ]
-        # read_only_fields = ['id', 'user', 'created_at', 'sold_sum_price', 'stock_sum_price', 'debt', 'received_profit', 'profit', 'paid', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'user', 'created_at', 'sold_sum_price', 'stock_sum_price', 'debt', 'received_profit', 'profit', 'paid', 'created_at', 'updated_at']
 
     def create(self, validated_data):
         items_data = validated_data.pop('product_list')
@@ -68,45 +68,6 @@ class OutcomeSerializer(serializers.ModelSerializer):
             dealer_group.save()
 
         return outcome
-    
-    def update(self, instance, validated_data):
-        items_data = validated_data.pop('product_list', None)
-        user = self.context['request'].user
-        dealer_group = user.dealer_groups.first()
-        client = validated_data.pop('client', instance.client)
-
-        validated_data.pop('user', None)
-        validated_data.pop('dealer_group', None)
-
-        total_sold = Decimal('0')
-        total_stock = Decimal('0')
-
-        if items_data is not None:
-            # Удаляем старые записи
-            instance.product_list.all().delete()
-
-            for item in items_data:
-                total_sold += item['quantity'] * item['sold_price']
-                total_stock += item['quantity'] * item['stock_price']
-                OutcomeItem.objects.create(outcome=instance, **item)
-
-            instance.sold_sum_price = total_sold
-            instance.stock_sum_price = total_stock
-            instance.profit = total_sold - total_stock
-            instance.debt = validated_data.get('debt', total_sold)
-            instance.paid = instance.debt == 0
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.client = client
-        instance.user = user
-        instance.dealer_group = dealer_group
-        instance.save()
-
-        return instance
-
-        
 
 
 
